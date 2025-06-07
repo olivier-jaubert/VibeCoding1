7cce119da191c20384b360d0e9002752aa2a512f
olivier-jaubert <jaubert.oli@gmail.com>
2025-06-07 17:34:55 +0100

    Update Working

--- src/Gallery.jsx
+++ src/Gallery.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Gallery.css";
 
 // Automatically import all jpg images from assets folder
@@ -6,14 +6,37 @@ const images = import.meta.glob('./assets/*.jpg', { eager: true });
 
 const imageList = Object.values(images).map((mod) => mod.default);
 
-const Gallery = () => (
-  <div className="gallery-container">
-    {imageList.map((src, idx) => (
-      <div className="gallery-item" key={idx}>
-        <img src={src} alt={`Gallery ${idx + 1}`} />
+const Gallery = () => {
+  const [selectedImg, setSelectedImg] = useState(null);
+
+  return (
+    <>
+      <h1 style={{ textAlign: "center" }}>
+        Personal Gallery<br />
+        Olivier's Friday Art journey
+      </h1>
+      <div className="gallery-container">
+        {imageList.map((src, idx) => (
+          <div className="gallery-item" key={idx}>
+            <img
+              src={src}
+              alt={`Gallery ${idx + 1}`}
+              onClick={() => setSelectedImg(src)}
+              style={{ cursor: "pointer" }}
+            />
+          </div>
+        ))}
       </div>
-    ))}
-  </div>
-);
+      {selectedImg && (
+        <div className="modal-overlay" onClick={() => setSelectedImg(null)}>
+          <div className="modal-content" onClick={e => e.stopPropagation()}>
+            <img src={selectedImg} alt="Large preview" />
+            <button className="modal-close" onClick={() => setSelectedImg(null)}>&times;</button>
+          </div>
+        </div>
+      )}
+    </>
+  );
+};
 
 export default Gallery;
